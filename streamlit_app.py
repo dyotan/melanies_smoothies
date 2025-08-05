c49d8465912674d69d1ea1bb18f321100e3dcd9b
dyotan <32848167+dyotan@users.noreply.github.com>
2025-08-04 23:29:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,10 @@ if ingredients_list:
    # st.stop()
     #if ingredients_string:
 
+  import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
